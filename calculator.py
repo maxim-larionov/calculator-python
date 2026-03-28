2b347b625df4dbcfb60510f401b0d8adc81bc631
Alisa and Max <Children@MacBook-Air-2.local>
2026-03-28 19:19:36 +0300

    Updated your calculator

--- calculator.py
+++ calculator.py
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import os
-import re
-
-'''clear function'''
-def clearTheConsole():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-alert = "Python calculator - Rafał Michałuszek 2016\n"
-programOn = 1
-
-while programOn:
-    clearTheConsole()
-    print(alert)
-    print('Type your mathematical operation:')
-
-    operation = input()
-
-    if operation is '':
-        alert = "--Please do not write a text--"
-        continue
-
-    if re.search('[a-zA-Z]', operation):
-        alert = "--Please do not write a text--"
-        continue
-
-    result = 0
-
-    operation = 'result = '+operation
-    exec(operation)
-
-    operation = ''
-    alert = "\n--The result is " + str(result) + "--"
-=======
 from math import *
 
 while True:
@@ -40,4 +6,3 @@ while True:
 
   except:
     print('Sorry?')
->>>>>>> f15c853 (first commit)
